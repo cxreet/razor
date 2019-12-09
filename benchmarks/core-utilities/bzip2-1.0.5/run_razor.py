1e23f8d79d8c33fdcd5f0a19311a90b3087b2fdf
chenxiong <qchenxiong3@gatech.edu>
2019-12-09 16:22:03 -0500

    added bzip2-1.0.5 testcases

---
+++ benchmarks/core-utilities/bzip2-1.0.5/run_razor.py
@@ -0,0 +1,102 @@
+#!/usr/bin/python
+from __future__ import print_function
+import os, subprocess, sys
+
+DRRUN = '../../../tracers/dynamorio/bin64/drrun'
+CLIENT = './logs/libcbr_indcall.so'
+
+def execute(cmd):
+    print('running ', cmd)
+    p = subprocess.Popen(cmd, shell=True)
+    p.communicate()
+
+def train():
+    BIN = './bzip2.orig'
+
+    for fname in os.listdir('train'):
+        fpath = os.path.join('train', fname)
+        # -c
+        cmd = BIN + ' -c < ' + fpath + ' > tmp.log'
+        cmd = DRRUN + ' -c ' + CLIENT + ' -- ' + cmd
+        print(cmd)
+        execute(cmd)
+
+def test():
+    BIN = './bzip2.orig_temp/bzip2.orig.debloated'
+
+    for fname in os.listdir('test'):
+        fpath = os.path.join('test', fname)
+        # -c
+        cmd = BIN + ' -c < ' + fpath + ' > tmp.log'
+        print(cmd)
+        execute(cmd)
+
+
+def debloat(logs_dir, prog_name):
+    execute("""python ../../../stitcher/src/merge_log.py %s %s""" % (logs_dir, prog_name))
+    execute("""mv %s/%s-trace.log ./""" % (logs_dir, prog_name))
+    execute("""python ../../../stitcher/src/instr_dumper.py ./%s-trace.log ./%s.orig ./instr.s""" % (prog_name, prog_name))
+    execute("""python ../../../stitcher/src/find_symbols.py ./%s.orig ./instr.s > ./callbacks.txt""" % (prog_name))
+    execute("""python ../../../stitcher/src/stitcher.py ./%s-trace.log ./%s.orig ./%s.s ./callbacks.txt""" % (prog_name, prog_name, prog_name))
+    execute("""python ../../../stitcher/src/merge_bin.py %s.orig %s.s""" % (prog_name, prog_name))
+
+def extend_debloat(prog_name):
+    execute("""python ../../../stitcher/src/heuristic/disasm.py ./%s.orig ./%s.org.asm """ % (prog_name, prog_name))
+    execute("""python ../../../stitcher/src/heuristic/find_more_paths.py ./%s.orig.asm ./%s-trace.log ./%s-extended.log""" % (prog_name, prog_name, prog_name))
+    execute("""python ../../../stitcher/src/instr_dumper.py ./%s-extended.log ./%s.orig ./instr.s""" % (prog_name, prog_name))
+    execute("""python ../../../stitcher/src/find_symbols.py ./%s.orig ./instr.s > ./callbacks.txt""" % (prog_name))
+    execute("""python ../../../stitcher/src/stitcher.py ./%s-extended.log ./%s.orig ./%s.s ./callbacks.txt""" % (prog_name, prog_name, prog_name))
+    execute("""python ../../../stitcher/src/merge_bin.py %s.orig %s.s""" % (prog_name, prog_name))
+
+def debloat_patch(prog_name):
+    #execute("""python ../../../stitcher/src/stitcher.py ./%s-trace.log ./%s.orig ./%s.s ./callbacks.txt""" % (prog_name, prog_name, prog_name))
+    execute("""python ../../../stitcher/src/merge_bin.py %s.orig %s.s""" % (prog_name, prog_name))
+
+def clean():
+    for fname in os.listdir('./'):
+        if fname == "run_razor.py":
+            continue
+        
+        if fname == 'test' or fname == 'train':
+            continue
+
+        execute('rm -rf ./' + fname)
+
+def usage():
+    print('python run_razor.py train|test|debloat|extend_debloat|debloat_patch\n')
+    sys.exit(1)
+
+def main():
+    if len(sys.argv) != 2:
+        usage()
+
+    if not os.path.exists("./logs"):
+        cmd = "mkdir -p ./logs"
+        execute(cmd)
+
+        cmd = "cp ../../../tracers/bin/libcbr_indcall.so ./logs/"
+        execute(cmd)
+    
+    if sys.argv[1] == 'train':
+        train()
+    
+    elif sys.argv[1] == 'test':
+        test()
+
+    elif sys.argv[1] == 'debloat':
+        debloat('logs', 'bzip2')
+
+    elif sys.argv[1] == 'extend_debloat':
+        extend_debloat('bzip2')
+
+    elif sys.argv[1] == 'debloat_patch':
+        debloat_patch('bzip2')
+
+    elif sys.argv[1] == 'clean':
+        clean()
+
+    else:
+        usage()
+
+if __name__ == '__main__':
+    main()
